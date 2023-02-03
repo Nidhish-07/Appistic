4e86e0648432010026cc7ec81e8fd5ea61b3ce51
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-02-03 11:01:28 +0530

    date range picker added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import { FaCarSide, FaTaxi, FaPlane, FaBed, FaCalendar } from "react-icons/fa"
 import { BsFillPersonFill } from "react-icons/bs"
 import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { format } from 'date-fns'
 const Header = () => {
 
     const [date, setDate] = React.useState([{
         startDate: new Date(),
-        endDate: null,
+        endDate: new Date(),
         key: 'selection'
     }])
+
+    const [openDate, setOpenDate] = React.useState(false)
+
+    const [openOptions, setOpenOptions] = React.useState(false)
+
+    const [options, setOptions] = React.useState({
+        adult: 1, children: 0, roomNo: 9
+    })
     return (
         <div className='bg-blue-600 text-white flex justify-center relative'>
             <div className='w-full max-w-[1024px] mt-5 mx-0 mb-24'>
@@ -44,14 +55,50 @@ const Header = () => {
                     </div>
                     <div className='flex gap-2 items-center '>
                         <FaCalendar color='#D3D3D3' />
-                        <span className='text-zinc-400 cursor-pointer'>check in</span>
+                        <span className='text-zinc-400 cursor-pointer' onClick={() => (setOpenDate(!openDate))}>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+                        {openDate && <DateRange
+                            editableDateInputs={true}
+                            onChange={item => setDate([item.selection])}
+                            moveRangeOnFirstSelection={false}
+                            ranges={date}
+                            className='absolute top-12'
+                        />}
                     </div>
                     <div className='flex gap-2 items-center '>
                         <BsFillPersonFill color='#D3D3D3' />
-                        <span className='text-zinc-400 cursor-pointer'>2 adults 1 room</span>
+                        <span className='text-zinc-400 cursor-pointer'>{`${options.adult} adult . ${options.children} children . ${options.roomNo} room`}</span>
+                        <div className='absolute top-12 bg-white text-zinc-500 rounded shadow-lg gap-2'>
+                            <div className='w-48 flex justify-between'>
+                                <span>Adult</span>
+                                <div className='flex items-center gap-2 text-xs text-black'>
+
+                                    <button className='w-7 h-7 border-2 border-solid border-blue-500 cursor-pointer' onClick={()=>optionHandler("adult","neg")}>-</button>
+                                    <span>1</span>
+                                    <button className='w-7 h-7 border-2 border-solid border-blue-500 cursor-pointer' onClick={()=>optionHandler("adult","pos")}>+</button>
+                                </div>
+                            </div>
+                            <div className='w-48 flex justify-between'>
+                                <span>Children</span>
+                                <div className='flex items-center gap-2 text-xs text-black'>
+
+                                    <button className='w-7 h-7 border-2 border-solid border-blue-500 cursor-pointer' onClick={()=>optionHandler("children","neg")}>-</button>
+                                    <span>0</span>
+                                    <button className='w-7 h-7 border-2 border-solid border-blue-500 cursor-pointer' onClick={()=>optionHandler("children","pos")}>+</button>
+                                </div>
+                            </div>
+                            <div className='w-48 flex justify-between'>
+                                <span>Room</span>
+                                <div className='flex items-center gap-2 text-xs text-black'>
+
+                                    <button className='w-7 h-7 border-2 border-solid border-blue-500 cursor-pointer' onClick={()=>optionHandler("room","neg")}>-</button>
+                                    <span>1</span>
+                                    <button className='w-7 h-7 border-2 border-solid border-blue-500 cursor-pointer' onClick={()=>optionHandler("room","pos")}>+</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className='flex gap-2 items-center '>
-                        <button className='bg-blue-400'>Search</button>
+                        <button className='bg-blue-400 p-2 rounded'>Search</button>
                     </div>
                 </div>
             </div>
